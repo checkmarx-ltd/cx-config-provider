1e792e184f471e0ed9a41e9bbb45ea264cf0f990
AlexeyK <alexey.kononov@checkmarx.com>
2020-09-01 12:00:52 +0300

    Added a private constructor.

--- src/main/java/com/checkmarx/configprovider/resource/Parser.java
+++ src/main/java/com/checkmarx/configprovider/resource/Parser.java
@@ -6,7 +6,8 @@ import com.typesafe.config.Config;
 import javax.naming.ConfigurationException;
 
 public class Parser {
-    
+    private Parser() {}
+
     public static Config parse(ConfigResource resource) throws ConfigurationException {
         return ((ParsableResource)resource).loadConfig();
     }
