18d122cc252706d583e8d2782bae51bb05ed431a
Ofer Skolsky <Ofer.Skolsky@checkmarx.com>
2020-09-15 12:50:59 +0300

    added log to resolving ResourceType by extentions

--- src/main/java/com/checkmarx/configprovider/dto/ResourceType.java
+++ src/main/java/com/checkmarx/configprovider/dto/ResourceType.java
@@ -2,7 +2,12 @@ package com.checkmarx.configprovider.dto;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public enum ResourceType {
     YAML("yaml", "yml"), JSON("json"), PROPERTIES("properties"), ENV_VARIABLES("env"), COMBINED();
 
@@ -13,9 +18,15 @@ public enum ResourceType {
     }
 
     public static ResourceType getTypeByExtention(String extention) {
+        UnaryOperator<ResourceType> logAndReturn = res -> {
+            log.info("extention {} is {}", extention, res);
+            return res;
+        };
+        log.info("resolving file type by extention");
         return Arrays.stream(values())
             .filter(type -> type.fileExtentions.contains(extention.toLowerCase())).findAny()
-            .orElse(COMBINED);
+            .map(logAndReturn)
+            .orElse(logAndReturn.apply(COMBINED));
         
     }
 }
